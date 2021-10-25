d00120f5d3dd67cca7c75c765296d8827f306555
NicolasDehes <nicolas.dehes@gmail.com>
2021-10-25 19:01:04 +0200

    beautiful(à peu près)

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const container = document.getElementById('content')
 
-const ajaxData = async(page) => {
+const ajaxData = async(page, state) => {
     var search = document.getElementById('searchInput').value
+    search = (state==="siret")? "siret/"+search : "full_text/"+search 
     container.innerHTML="<div class=\"loader\"></div>" //Petite animation de chargement en attendant la promesse
     const parseJSON = resp => (resp.json ? resp.json() : resp);
 
     const checkStatus = resp => {
       if (resp.status >= 200 && resp.status < 300) {
         return resp;
+      }
+      if (resp.status === 404){
+          throw "Aucune entreprise trouvée"
       }
       return parseJSON(resp).then(resp => {
         throw resp;
@@ -19,51 +23,71 @@ const ajaxData = async(page) => {
 
     try {
         if(search=="") throw "Besoin d\'un élément à chercher"
-        var data = await fetch(`https://entreprise.data.gouv.fr/api/sirene/v1/full_text/${search}?page=${page}`, {
+        var data = await fetch(`https://entreprise.data.gouv.fr/api/sirene/v1/${search}?page=${page}`, {
             method: 'GET',
             headers: {'Content-Type': 'application/json'},
         })
           .then(checkStatus)
           .then(parseJSON);
+        container.innerHTML=`<p class=\"text-secondary text-center m-3\">Nombre de résultats ${data.total_results} - Page: ${data.page}</p>`
         var beautifuler=`
-            <div class='container text-center'>
-                <div class='row p-5'>
+            <div class='container text-center border border-primary'>
+                <div class='row p-3 border-primary border-bottom'>
                     <div class='col'>Siret</div>
                     <div class='col'>Nom</div>
                     <div class='col'>...</div>
                 </div>`;
-        container.innerHTML=`<p class=\"text-secondary text-center m-3\">Nombre de résultats ${data.total_results} - Page: ${data.page}</p>`
-        var counter=0
-        data.etablissement.map(entreprise => {    
+        if(state=="nom"){
+            var counter=0
+            data.etablissement.map(entreprise => {    
+                var date_entreprise = entreprise.date_creation
+                beautifuler+=`<div class="row text-center pt-2">
+                    <div class='col'>${entreprise.siret}</div>
+                    <div class='col'>${entreprise.nom_raison_sociale}</div>
+                    <div class='col'><button class='m-auto btn btn-outline-info' onclick="infoHandler(this,${counter});">Détails</button></div>
+                </div>`
+                /* Détails caché */
+                +
+                `<div class='container border border-info mt-1' id='info-${counter}' hidden>
+                    <p>Catégorie entreprise: ${entreprise.categorie_entreprise}</p>
+                    <p>Créée le: ${date_entreprise.substring(6,8)} - ${date_entreprise.substring(4,6)} - ${date_entreprise.substring(0,4)}</p>
+                    <p>Adresse: ${entreprise.geo_adresse}</p>
+                    <p>Activité principale: ${entreprise.libelle_activite_principale}</p>
+                </div>`
+                counter++
+            })
+        } else {
+            var entreprise = data.etablissement
             var date_entreprise = entreprise.date_creation
-            beautifuler+=`<div class="row text-center">
+            beautifuler+=`<div class="row text-center pt-2">
                 <div class='col'>${entreprise.siret}</div>
                 <div class='col'>${entreprise.nom_raison_sociale}</div>
-                <div class='col'><button class='m-3' onclick="infoHandler(this,${counter});">Détails</button></div>
+                <div class='col'><button class='m-auto btn btn-outline-info' onclick="infoHandler(this,${counter});">Détails</button></div>
             </div>`
             /* Détails caché */
             +
-            `<div class='container' id='info-${counter}' hidden>
+            `<div class='container border border-info mt-1' id='info-${counter}' hidden>
                 <p>Catégorie entreprise: ${entreprise.categorie_entreprise}</p>
                 <p>Créée le: ${date_entreprise.substring(6,8)} - ${date_entreprise.substring(4,6)} - ${date_entreprise.substring(0,4)}</p>
                 <p>Adresse: ${entreprise.geo_adresse}</p>
                 <p>Activité principale: ${entreprise.libelle_activite_principale}</p>
             </div>`
-            counter++
-        })
-        beautifuler+=`
-        <div class="text-secondary">
-            ${(data.page!=1)? `
-                <p onclick=\"ajaxData(${page-1})\">\<</p>
-                <p onclick=\"ajaxData(1);\">1</p> \-
-                ` : ''}
-            <p>${data.page}</p>
-            ${(data.page!=data.total_pages)? `
-                \- <p onclick="ajaxData(${data.total_pages})">${data.total_pages}</p>
-                <p onclick=\"ajaxData(${page+1})\">\></p>
-                ` : ''}
-        </div>
-        </div>`// Close the content container
+        }
+        if(state=="nom"){
+                beautifuler+=`
+            <div class="text-secondary border-top border-primary mt-1">
+                ${(data.page!=1)? `
+                    <p class="d-inline-block" onclick=\"ajaxData(${page-1})\">\<</p>
+                    <p class="d-inline-block" onclick=\"ajaxData(1);\">1</p> \-
+                    ` : ''}
+                <p class="d-inline-block text-dark">${data.page}</p>
+                ${(data.page!=data.total_pages)? `
+                    \- <p class="d-inline-block" onclick="ajaxData(${data.total_pages})">${data.total_pages}</p>
+                    <p class="d-inline-block" onclick=\"ajaxData(${page+1})\">\></p>
+                    ` : ''}
+            </div>`
+        }
+        beautifuler+='</div>'// Close the content container
         container.innerHTML+=beautifuler
     } catch (error) {
         console.error(error)
